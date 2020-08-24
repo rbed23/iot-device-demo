3ff6c56affd307df0add22e6acc5b5470020a7aa
Ryan B <rbed23@gmail.com>
2020-08-24 15:48:17 +0300

    changed sensor type

--- board_setup.py
+++ board_setup.py
@@ -1,11 +1,12 @@
 from gpiozero import LED, Button, LightSensor
+from gpiozero import DigitalInputDevice
 from time import sleep, time
 
 
 board = LED(4)
 led_main = LED(18)
 button = Button(2)
-sensor = LightSensor(27)
+sensor = DigitalInputDevice(27)
 
 
 board.on()
