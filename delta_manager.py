18bfae5faa0f0f6414db9ce1d674ed294be9055b
Ryan B <rbed23@gmail.com>
2020-08-24 16:50:26 +0300

    cupdated delta manager

--- delta_manager.py
+++ delta_manager.py
@@ -3,7 +3,7 @@ from board_setup import *
 def led_main_handler(action):
     if action == 'on':
         led_main.on()
-    elif action == 'off'
+    elif action == 'off':
         led_main.off()
     else:
         pass
